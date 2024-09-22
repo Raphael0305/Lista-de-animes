5a6500d578c8b4cffc9cea5fe8383d90dbe10321
Raphael <162832824+Raphael0305@users.noreply.github.com>
2024-09-22 18:14:11 -0300

    Reformulando Código
    Como aprendi a usar Struct e melhorei meu uso de vector refiz o código, ainda falta a opção "salvar" e "carregar", provavelmente vou tirar o  salvar e deixar meio que ''automaticamente'' o salvamento.
    Outro dia termino, não estou tendo muito tempo ;-;
    ps. comentei bastante o código, tenho pegado essa pratica no estágio, e a indentação também, mas é meio difícil fazer isso no DEV C++ tenho que mudar para o VScode logo

--- Lista Anime/main.cpp
+++ Lista Anime/main.cpp
@@ -4,30 +4,37 @@
 #include <algorithm>
 #include <sstream>
 #include <fstream>
+#include <conio.h>
+
 using namespace std;
 
-void menu ();
-void cadastrarAnime (vector<pair<string, string> >& animes); // ADICIONAR ANIMES 
-void listarAnime (const vector<pair<string,string > >& animes, vector<pair<string, int>>& top10); // MOSTRAR OS ANIMES ADICIONADOS E O TOP 10 
-void excluirAnime (vector<pair<string, string >>& animes, string& nomeRemover); // EXLUIR ANIME DA LISTA PRINCIPAL *ADICIONAR ANIMES*
-void listartop10 (vector<pair<string, int>>& top10); // PARA CADASTRAR O TOP10
-void exluirTop10 (vector<pair<string, int>>& top10, int& rank); // EXCLUIR ANIME DA LISTA "TOP10"
-void salvarAnimes(vector<pair<string, string >>& animes); // Salvar Animes(salva em um arquivo txt);
-void carregarAnimes(vector<pair<string, string>>& animes); // carregar anime
+struct animeLista{
+	string nomeAnime;
+	string Comentario;
+};
 
+struct listaTop10{
+	int rank;
+	string nomeTop;
+};
+//  AUXILIARES
+void systemClear();
+void systemPause();
+//*******************
+// FUNÇÕES PIRNCIPAIS
+void menu ();
+void cadastrarAnime (vector<animeLista>& anime);
+void verTodosAnimes (vector<animeLista>& anime, vector<listaTop10>& top10);
+void addTOP10(vector<listaTop10>& top10); // add TOP 10 
+void excluirAnime (vector<animeLista>& anime, vector<listaTop10>& top10);
 
 int main(int argc, char** argv) {
+	int op;
 	setlocale(LC_ALL, "portuguese");
-	string nomeRemover; // parametro para excluir do vetor anime
-	vector<pair<string, string > > animes; // vetor anime
-	vector<pair<string, int >> top10; // vetor top10
-	bool encontrado = false;
-	int op, numerorank, ope; // op = opção menu. numero rank = paramentro pra excluir algo do vetor top10 .   ope = opção dentro do case excluir  
-	 
-	 
-	 carregarAnimes(animes);
-	 
-	while(true){
+	vector<animeLista> anime;
+	vector<listaTop10> top10;
+	
+	do{
 		
 		system("cls");
 		menu ();
@@ -37,100 +44,37 @@ int main(int argc, char** argv) {
 		switch(op){
 			
 			case 1:
-				system("cls");
-				cadastrarAnime (animes);
-				system("pause");
+					systemClear();
+					cadastrarAnime (anime);
+					systemPause();
 				break;
 			case 2:
-				listarAnime (animes, top10);
-				system("pause");
+					systemClear();
+					verTodosAnimes (anime,top10);
+					systemPause();
 				break;
 			case 3:
-				listartop10 (top10);
-				system("pause");
+					systemClear();
+					addTOP10(top10);
+					systemPause();
 				break;
 			case 4:
-				
-				cout << "De qual você vai excluir? \n";
-				cout << " 1 - Lista animes \n";
-				cout << " 2 - Top 10 \n";
-				cin >> ope;
-				switch(ope){
-					case 1:
-						cout << "Qual o nome do anime que deseja excluir? \n";
-						cin.ignore();
-						getline(cin,nomeRemover);
-						
-						encontrado = false;
-						
-						for (auto& nomes : animes){
-							if(nomeRemover == nomes.first){
-								encontrado = true;
-								cout << "Deseja mesmo remover: " << nomes.first << " ? (S/N)\n";
-								char pergunta;
-								cin >> pergunta;
-								
-								if(pergunta == 'S' || pergunta == 's'){
-									excluirAnime (animes, nomeRemover);
-									cout << "Anime excluído com sucesso!\n";
-									break;
-								}else {
-									cout << "Ação cancelada\n";
-									encontrado = true;  // Para evitar a mensagem de erro.
-									break;
-								}
-							}
-						}
-
-						if(!encontrado){
-							cout << "Anime não encontrado\n";
-						}
-						break;
-					case 2:
-						int rankExcluir;
-						
-						cout << "Em qual rank está o anime que você deseja excluir? ";
-						cin >> rankExcluir;
-						encontrado = false;
-						
-						for(auto& numero : top10){
-							if(rankExcluir == numero.second){
-								encontrado = true;
-								cout << "Deseja mesmo apagar o anime: " << numero.first << "? (S/N)\n";
-								char confirme;
-								cin >> confirme;
-								if(confirme == 'S' || confirme == 's'){
-									exluirTop10 (top10, rankExcluir);
-									cout << "Anime excluído com sucesso!\n";
-								}else {
-									cout << "Ação cancelada\n";
-								}
-								break;  // Sai do loop ao encontrar e confirmar exclusão
-							}
-						}
-
-						if(!encontrado){
-							cout << "Não foi possível encontrar esse rank\n";
-						}
-						break;
-					default:
-						cout << "Essa opção não existe\n";
-						break;
-				}
-				system("pause");
+					systemClear();
+					excluirAnime (anime, top10);
+					systemPause();
 				break;
 			case 5:
-				salvarAnimes(animes);
+
 				system("pause");
 				break;
 			case 6:
 				return 0;
 				break;		
 			default :
-				cout << "Essa opção não é válida\n";
+				cout << "Essa opção não é valida\n";
 				break;		
 		}
-	}
+	}while(true);
 		
 	
 	return 0;
@@ -148,133 +92,336 @@ void menu (){ //mostrar o menu
     cout << "|=================================|\n";
     cout << "Escolha uma opção: ";
 }
-
-
-void cadastrarAnime (vector<pair<string, string > >& animes){ 
-
-	string nomeAnime, comentario;
+//***********************************************************************************************************
+//Função para cadastrar animes
+void cadastrarAnime (vector<animeLista>& anime){ 
+	cout << "+-------------------+\n";
+	cout << "| CADASTRO DE ANIME |\n";
+	cout << "+-------------------+\n";
+	cout << "\n";
+	cout << "Press ENTER para voltar\n";
 	
+	animeLista addAnime;
+	
+	cout << "\nDigite o nome do Anime\n";
 	cin.ignore();
-	cout << "Qual o nome do Anime?\n";
-	getline(cin, nomeAnime);
+	getline (cin, addAnime.nomeAnime);
 	
-	cout << "Escreva um comentário\n";
-	getline(cin, comentario);  // getline para pegar palavras com distanciamento entre elas
-	
-	animes.push_back(make_pair(nomeAnime, comentario)); // para alocar os valores dentro do vetor 
-	cout << "\n";
-	cout << "Cadastro completo!\n";
-	cout << "Quantidade: " << animes.size();
-	cout << "\n";
-}
-
-void listarAnime (const vector<pair<string, string > >& animes, vector<pair<string, int>>& top10){
-	int op;
-	if(animes.empty() && top10.empty()){  // if para saber se o vetor está vazio ou não, se for true retorna o cout 
-		cout << "Não tem nada cadastrado\n";
+  	if(addAnime.nomeAnime.empty()){
+		cout << "Voltando ao menu...\n";
 		return;
 	}
 	
-	cout << "Deseja ver qual lista? \n";
-	cout << "\n";
-	cout << "1 - Listar todos \n";
-	cout << "2 - Listar top 10 \n";
-	cin >> op;
-	
-	switch(op){
-		case 1:
-				for(const auto& lista : animes){ // for para rodar todos os elementos do vetor
-					cout << "\n";
-					cout << "===============================================================================\n";
-					cout << "ANIME: " << lista.first << " | " << "Comentário: " << lista.second << "\n";
-					cout << "===============================================================================\n";
-				}
-			break;
-		case 2: 
-			for(const auto& lista2 : top10){
-					cout << "\n";
-					cout << "===============================================================================\n";
-					cout << "ANIME: " << lista2.first << " | " << "RANK: " << lista2.second << "\n";
-					cout << "===============================================================================\n";
-				}
-			break;
-		default :
-					cout << "Não existe essa opção\n";
-			break;
-	}
-}
- 
-void listartop10 (vector<pair<string, int>>& top10){  // está listar mas é para escrever o top10
-	string nome;
-	int rank;
-	
+	cout << "\nComentario\n";
 	cin.ignore();
-	cout << "Digite o nome do anime:\n";
-    getline(cin, nome);  
-
-    cout << "\nEm qual rank vai ficar?\n";
-    cin >> rank;
-
-    top10.push_back(make_pair(nome, rank)); // alocando o valor das variáveis no vetor top10
-
-    for (const auto& anime : top10) {  // rodando a lista de animes
-        cout << "Nome do anime: " << anime.first << " | Rank: " << anime.second << "\n";
-        break; 
-    }
-}
-
-void excluirAnime (vector<pair<string, string >>& animes, string& nomeRemover){ // excluir da lista principal 
-
-	auto lixeira = remove_if (animes.begin(), animes.end(), [&nomeRemover] (const pair<string,string>& p){
-		return p.first == nomeRemover;
-	});// soft função para alocar o valor selecionado em rank para a última posição do vetor 
+	getline (cin, addAnime.Comentario);
 	
-	animes.erase(lixeira, animes.end()); // para excluir o último comando do vetor,
-
-}
-
-void exluirTop10 (vector<pair<string, int>>& top10, int& rank){  // excluir do top10
-	
-	 auto lixeira = remove_if(top10.begin(), top10.end(), [&rank](const pair<string,int>& r){ // soft função para alocar o valor selecionado em rank para a última posição do vetor 
-		return r.second == rank;
-	});
-	
-	top10.erase(lixeira, top10.end()); // para excluir o último comando do vetor,
-}
-
-
-void salvarAnimes(vector<pair<string, string >>& animes){
-	
-	ofstream arquivo("animes.txt");
-	
-	if(arquivo.is_open()){
-		for(const auto& anime : animes){
-			arquivo << anime.first << " | " << anime.second << "\n";
-		}
-		arquivo.close();
-		cout << "Animes salvos com sucesso!\n";
-	}else {
-		cout << "Não foi possível salvar os animes. \n";
+	if(addAnime.Comentario.empty()){
+		cout << "Voltando ao menu...\n";
+		return;
 	}
 	
+	
+	
+	
+	anime.push_back(addAnime);
+	cout << "\n Anime Cadastrado\n";
+	cout << "\n";
+}
+//***********************************************************************************************************
+//==================================
+// LISTA PARA VER TODOS OS ANIMES  |
+//==================================
+
+void verTodosAnimes (vector<animeLista>& anime, vector<listaTop10>& top10){
+	int OP;
+	bool VERIF = true;
+	do {
+		systemClear();
+		cout << "+------------------+\n";
+		cout << "| LISTA DE ANIMES  |\n";
+		cout << "+------------------+\n";
+
+	                                              // loop para varias coisinhas
+		cout << "Qual lista\n";
+		cout << " 1 - Lista de Animes\n";
+		cout << " 2 - TOP 10\n";
+		cout << " 3 - Voltar\n";
+		cin >> OP;
+	
+		switch(OP){
+			case 1:
+				if(anime.empty()){      // verifica se tem algum valor no vetor
+					cout << "\nNão existe anime cadastrado\n";
+					systemPause();
+					VERIF = true;
+					break;
+				}else {
+					for(const auto& animes : anime){   //mostra os valores
+						cout << "===================================================\n";
+						cout << "Nome do Anime: " << animes.nomeAnime << "\n";
+						cout << "Comentario : " << animes.Comentario << "\n";
+						cout << "===================================================\n";
+					}	
+				}
+				VERIF = false;
+				break;
+			case 2:
+				if(top10.empty()){
+					cout << "\nNão existe anime cadastrado\n";
+					systemPause();
+					VERIF = true;
+					break;
+				}else{
+					for(const auto& top : top10){
+						cout << "Rank: " << top.rank << "  |   Nome: " << top.nomeTop << "\n";
+						cout << "\n";
+						VERIF = false;
+					}
+				}	
+				break;
+			case 3: 
+				return;
+				break;
+			default:
+					cout << "Essa opção não existe \n";
+					VERIF = true;
+				break;
+		}
+	
+	}while(VERIF);	
+}
+//***********************************************************************************************************
+//====================
+// ADICONAR TOP10    |
+//===================
+void addTOP10(vector<listaTop10>& top10){
+
+	cout << "+==========================+\n";
+	cout << "|   ADICIONANDO O TOP 10   |\n";
+	cout << "+==========================+\n";
+	cout << "\n";
+	listaTop10 top; //Declarando a varaiavel que vai pegar os valores 
+	string input; // cirando um input auxiliar pra tranformar em int 
+	cout << "\n Press ENTER para voltar ao menu\n";
+	cout << "\n";
+	
+	cout << "Nome do anime: ";
+	cin.ignore();
+	getline(cin, top.nomeTop);
+	
+	if(top.nomeTop.empty()){     // caso queiraa voltar ao menu sem digitar nada
+		cout << "Voltando ao Menu\n";
+		return;
+	}
+	
+	cout << "Rank do anime: ";
+	cin >> input;
+	
+	if(input.empty()){
+		cout << "Voltando ao Menu\n";
+		return;
+	}
+	
+	top.rank = stoi(input);   // fiz esse para que transformar valores string em inteiros, tenho que colocar o try e catch ainda pra ficar melhor\n
+	
+	top10.push_back(top); // mandando os valores para o vector
 }
 
-void carregarAnimes(vector<pair<string, string>>& animes) {
-    ifstream arquivo("animes.txt");
-    if (arquivo.is_open()) {
-        string linha;
-        while (getline(arquivo, linha)) {
-            stringstream ss(linha);
-            string nomeAnime, comentario;
-            if (getline(ss, nomeAnime, '|')) { // Modificado para separar por '|'
-                getline(ss, comentario);
-                animes.push_back(make_pair(nomeAnime, comentario));
-            }
-        }
-        arquivo.close();
-        cout << "Animes carregados com sucesso!\n";
-    } else {
-        cout << "Não foi possível abrir o arquivo de animes. Verifique se o arquivo existe.\n";
-    }
-}
+//***********************************************************************************************************
 
+void excluirAnime (vector<animeLista>& anime, vector<listaTop10>& top10){
+	int op;
+	string nomeExcluir;
+	bool defaul = true; // criado para verificar um valor DEFAULT para voltar as opções
+/*	
+	if(anime.empty() && top10.empty()){   // verificação de entrada
+		cout << "\n";
+		cout << "\nNão tem anime cadastrado em nenhuma lista\n";
+		cout << "\n";
+		return;
+	}
+*/
+	do {  // para caso o valor default seja ativado
+		systemClear();
+		
+		cout << "Qual lista você vai excluir ?\n";
+		cout <<" 1 - Lista de Animes \n";
+		cout <<" 2 - TOP 10 \n";
+		cout <<" 3 - Voltar \n";
+		cin >> op;
+		
+		//variaveis do witch 
+		bool voltar = true; // tentei cirar dentro do case 1, ja que é usada la, mas tava dando erro então fui pelo caminho facil hehe
+		bool encontrado = true;		
+		int resp;
+		//variavel case 2
+		int rankExcluir;
+		bool defaultExcluir = true;
+		bool voltar2;
+		int escolhaExcluir;		
+		switch(op){
+			case 1 : 
+					if(anime.empty()){
+						cout << "Não tem anime cadastrado na Lista\n";
+						systemPause();
+						defaul = true;
+					}
+				do {
+					defaul = false;
+					systemClear();
+					cout << "+-------------------+\n";
+					cout << "|   Excluir Anime   |\n";
+					cout << "+-------------------+\n";
+					voltar = false;  //  so esta no case 1, voltar caso de fault seja ativado no case
+					encontrado = false; // bolano para entrar o nome do anime e mandar a messagem de não encontrado
+					cout << "Press ENTER para voltar\n";
+					cout << "\n";
+					
+					cout << "\nNome do anime que deseja excluir \n";
+					cin.ignore();
+					getline(cin, nomeExcluir);
+					
+					if(nomeExcluir.empty()){  // para voltar ao menu
+						cout <<" Voltando.. \n";
+						return;
+					}
+				
+					for (auto animes : anime){		// Encontrar o anime
+						if (nomeExcluir == animes.nomeAnime){ //para encontrar o anime
+							
+							cout << "Deseja realmente excluir " << animes.nomeAnime << "? \n";
+							cout << " 1 - Sim\n";
+							cout << " 2 - Não\n";
+							cin >> resp; //resposta para o switch
+								
+							if(resp == 's'){  // tratamento para o switch aceitar as respsota
+								resp = 'S';
+							}else if(resp == 'n'){
+								resp = 'N';
+							}
+							
+							switch(resp){
+								case 1:
+									    anime.erase(remove_if(anime.begin(), anime.end(), [&nomeExcluir](const animeLista& a) { 
+									    
+                						return a.nomeAnime == nomeExcluir;  // Comparando o nome do anime
+                						
+            							}), anime.end());
+            							
+            							cout << "Excluido com sucesso!\n";
+									break;
+								case 2:
+									cout << "Ação cancelada\n";
+									return;
+									break;
+								default:
+									cout << "Essa opção não existe\n";
+									systemPause();
+									voltar = true;
+									break;
+							}	// fim do switch			
+							encontrado = true;  // boleano avisando que foi encontrado no for o nome do anime 
+						} //fim do IF verificar se o nome igual ao escolhido
+					} // Fim do LOOP for para rodar todos os nomes de animes
+					
+					if (encontrado != true){
+						cout << "\n";
+						cout << "\nAnime não encontrado\n";
+						cout <<" Voltando...\n";
+						cout << "\n";
+						systemPause();
+						voltar = true;
+					}
+				}while(voltar);
+				// CARA ISSO FICOU ENORME, EU TIVE QUE COMENTAR TUDO PQ ATÉ EU TAVA PERDIDO KKKKKKKKKKKKKKKKKKKK
+				break;
+			case 2:
+/*
+				if(top10.empty()){
+					cout << "Não tem anime adicionado no TOP 10\n";
+					cout << "\n";
+					return;
+				}
+				*/
+				do {
+					voltar2 = false;
+					defaultExcluir = false;
+					cout <<"Qual o rank do anime que deseja excluir ? \n";
+					cin >> rankExcluir;
+					
+					for(auto procura : top10){
+						if (rankExcluir == procura.rank){
+							encontrado = true;
+							do{
+								cout << "Deseja mesmo excluir o anime : " << procura.nomeTop << "? \n";
+								cout << "1 - Sim \n";
+								cout << "2 - Não \n";
+								cin >> escolhaExcluir;
+							
+								switch(escolhaExcluir){
+									case 1:
+									    top10.erase(remove_if(top10.begin(), top10.end(), [&escolhaExcluir](const listaTop10& a) { 
+									    
+                						return a.rank == escolhaExcluir;  // Comparando o nome do anime
+                						
+            							}), top10.end());
+										
+										cout << "Anime excluido com sucesso!\n";
+										cout << "\n";
+										return;										
+										break;
+									case 2 :
+										cout << "Ação cancelada\n";
+										return;
+										break;
+									default :
+										cout << "Não tem essa opção dog\n";
+										systemPause();
+										defaultExcluir = true;
+										break;
+								}
+							}while(defaultExcluir);	
+						}
+					}		
+				}while(voltar2);		
+				break;
+			case 3:
+				return;
+				break;
+			default:
+				cout <<"Essa opção não existe dog\n";
+				systemPause();
+				defaul = true;
+				break;
+		}
+		if(encontrado != true){
+			cout << "Esse rank não esta cadastrado\n";
+			systemPause();
+		}
+	}while(defaul);
+
+}
+//**************************
+// AUXILIARES PARA SYSTEM  |
+//**************************
+//***********************************************************************************************************
+// Função para pausar o sistema
+void systemPause() {
+#ifdef _WIN32
+    system("pause"); // Windows
+#else
+cout << "Pressione enter para continuar...";
+cin.ignore(); // Linux 
+#endif
+}
+//***********************************************************************************************************
+// Função para limpar a tela
+void systemClear() {
+#ifdef _WIN32
+    system("cls"); // Windows
+#else
+    system("clear"); // Linux (ou outros sistemas Unix)
+#endif
+}
+//***********************************************************************************************************
